1a5da0fa9f20aea4250110b6e6cf3518de54c3f7
Zan Javed <{ID}+{username}@users.noreply.github.com>
2020-04-12 20:44:57 -0700

    added 10 products, 5 recently visited pages, top 5 popular pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Finder\Finder;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
@@ -82,6 +84,185 @@ class DefaultController extends AbstractController
     }
 
     /**
+     * @Route("/rental")
+     */
+    public function rental(Request $request) {
+        // if(isset($user))
+        //     return $this->render('default/rental.html.twig', ['login' => true]);
+        // else
+        //     return $this->render('default/rental.html.twig');
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/rental.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/rental.html.twig');
+
+        // $response->headers->setCookie(new Cookie('test', 'testValue'));
+        $this->setRecentCookie($request, $response, 'Rental');
+        $this->setPopularCookie($request, $response, 'Rental');
+        return $response;
+    }
+
+    /**
+     * @Route("/buy/car")
+     */
+    public function buyCar(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/buyCars.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/buyCars.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Buy A Car');
+        $this->setPopularCookie($request, $response, 'Buy A Car');
+        return $response;
+    }
+
+    /**
+     * @Route("/sell/car")
+     */
+    public function sellCar(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/sellCars.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/sellCars.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Sell A Car');
+        $this->setPopularCookie($request, $response, 'Sell A Car');
+        return $response;
+    }
+
+    /**
+     * @Route("/buy/accessory")
+     */
+    public function buyAccessory(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/buyAccessory.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/buyAccessory.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Buy Accessories');
+        $this->setPopularCookie($request, $response, 'Buy Accessories');
+        return $response;
+    }
+
+    /**
+     * @Route("/sell/accessory")
+     */
+    public function sellAccessory(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/sellAccessory.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/sellAccessory.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Sell Accessories');
+        $this->setPopularCookie($request, $response, 'Sell Accessories');
+        return $response;
+    }
+
+    /**
+     * @Route("/buy/parts")
+     */
+    public function buyParts(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/buyParts.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/buyParts.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Buy Car Parts');
+        $this->setPopularCookie($request, $response, 'Buy Car Parts');
+        return $response;
+    }
+
+    /**
+     * @Route("/sell/parts")
+     */
+    public function sellParts(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/sellParts.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/sellParts.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Sell Car Parts');
+        $this->setPopularCookie($request, $response, 'Sell Car Parts');
+        return $response;
+    }
+
+    /**
+     * @Route("/tune")
+     */
+    public function tune(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/tune.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/tune.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Tune Your Car');
+        $this->setPopularCookie($request, $response, 'Tune Your Car');
+        return $response;
+    }
+
+    /**
+     * @Route("/repair")
+     */
+    public function repair(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/repair.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/repair.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Repair Your Car');
+        $this->setPopularCookie($request, $response, 'Repair Your Car');
+        return $response;
+    }
+
+    /**
+     * @Route("/learn")
+     */
+    public function learn(Request $request) {
+        $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/learn.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/learn.html.twig');
+
+        $this->setRecentCookie($request, $response, 'Learn');
+        $this->setPopularCookie($request, $response, 'Learn');
+        return $response;
+    }
+
+    /**
+     * @Route("/recent")
+     */
+    public function recent(Request $request) {
+        $recent = $this->getRecentCookie($request);
+        $user = $this->getUser();
+        if(isset($user))
+            return $this->render('default/recent.html.twig', ['login' => true, 'recent' => $recent]);
+        else
+            return $this->render('default/recent.html.twig', ['recent' => $recent]);
+    }
+
+    /**
+     * @Route("/popular")
+     */
+    public function popular(Request $request) {
+        $popular = $this->getPopularCookie($request);
+        $user = $this->getUser();
+        if(isset($user))
+            return $this->render('default/popular.html.twig', ['login' => true, 'popular' => $popular]);
+        else
+            return $this->render('default/popular.html.twig', ['popular' => $popular]);
+    }
+
+    /**
      * @Route("/loggedin", name="loggedin")
      */
     public function loggedIn() {
@@ -112,6 +293,31 @@ class DefaultController extends AbstractController
             return $this->render('default/contacts.html.twig', ['contacts' => $contacts, 'login' => true]);
         else
             return $this->render('default/contacts.html.twig', ['contacts' => $contacts]);
+    }
+    
+    /**
+     * @Route("/clear")
+     */
+    public function clear(Request $request) {
+       $user = $this->getUser();
+        if(isset($user))
+            $response = $this->render('default/index.html.twig', ['login' => true]);
+        else
+            $response = $this->render('default/index.html.twig');
+
+        $response->headers->clearCookie('recent');
+        $response->headers->clearCookie('popular');
+        return $response;
+    }
+
+    /**
+     * @Route("/cookies")
+     */
+    public function cookies(Request $request) {
+        $cookies = $request->cookies;
+        print_r(unserialize($cookies->get('recent')));
+        print_r(unserialize($cookies->get('popular')));
+
     }
 
     /**
@@ -144,5 +350,58 @@ class DefaultController extends AbstractController
             // echo($contents);
             return $contents;
         }
+    }
+
+    public function setRecentCookie(Request $request, Response $response, String $page) {
+        $cookies = $request->cookies;
+        if (!$cookies->has('recent')) {
+            // var_dump($cookies->get('test'));
+            // $request->header->setCookie('recent', serialize(array()), time() + 3600);
+            $recent = array();
+        } else
+            $recent = unserialize($cookies->get('recent'));
+
+        if(count($recent) > 4) {
+            unset($recent[0]);
+            $recent = array_values($recent);
+        }
+        array_push($recent, $page);
+        $response->headers->setCookie(new Cookie('recent', serialize($recent), time() + 3600));
+    }
+
+    public function getRecentCookie(Request $request) {
+        $cookies = $request->cookies;
+        if (!$cookies->has('recent')) 
+            $recent = array();
+        else
+            $recent = unserialize($cookies->get('recent'));
+
+        return $recent;
+    }
+
+    public function setPopularCookie(Request $request, Response $response, String $page) {
+        $cookies = $request->cookies;
+        if (!$cookies->has('popular')) {
+            $popular = array();
+        } else
+            $popular = unserialize($cookies->get('popular'));
+
+        if(!array_key_exists($page, $popular))
+            $popular[$page] = 0;
+
+        $popular[$page]++;
+        $response->headers->setCookie(new Cookie('popular', serialize($popular), time() + 3600));
+    }
+
+    public function getPopularCookie(Request $request) {
+        $cookies = $request->cookies;
+        if (!$cookies->has('popular')) 
+            $popular = array();
+        else
+            $popular = unserialize($cookies->get('popular'));
+
+        arsort($popular);
+        $top5 = array_slice($popular, 0, 5, true);
+        return array_keys($top5);
     }
 }
